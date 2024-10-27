8391bfef73380abc888d1784aca93f96f3a6e44b
BelalBasal <basdibrahim@gmail.com>
2024-10-27 17:15:54 +0300

    Reverse

---
+++ Reverse/Program.cs
@@ -0,0 +1,14 @@
+﻿using Shared;
+
+namespace Reverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] fruits = { "apricot", "orange", "banana", "mango", "apple", "grape", "strawberry" };
+            var ReversedFruits = fruits.Reverse();
+            ReversedFruits.Print("Reversed Fruits ");
+        }
+    }
+}
